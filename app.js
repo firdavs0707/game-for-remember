847d32f85b697394e2bdc7e148467426e5cc9c07
firdavs0707 <firdavs0.4.2.0.0.7>
2022-07-24 13:07:47 +0500

    add codes upgrade

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-const suu = 'Siz notogri yozdingiz!!!';
+const suu = 'Siz notogri sonlarni kirittingiz!!!   Iltimos qayta urinib koring';
 
-const suu3 = 'Siz togri yozdingiz!'
+const suu3 = 'Siz togri sonlarni kirittingiz!  Sizning xotirangiz juda yaxshi ekan'
 
-const res = Math.random();
+const res = Math.floor(Math.random() * 100000000);;
 
 alert('eslab qoling:   ' + res)
 
 const check = prompt('Eslab qolganingizni yozing');
 
-if (check == res){
-  document.getElementById('demo').innerHTML = suu3;
-} else {
-  document.getElementById('demo').innerHTML = suu;
+if (check.length >= 9) {
+  document.getElementById('demo').innerHTML = 'Siz kop son kiritib yubordingiz!';
+}else if (check.length == 8) {
+  if (check == res){
+    document.getElementById('demo').innerHTML = suu3;
+  } else {
+    document.getElementById('demo').innerHTML = suu;
+  }
+}else if (check.length < 8) {
+    document.getElementById('demo').innerHTML = 'Sizga korsatilgan sonlardan kamroq son kirittingiz!';
+}else {
+  document.getElementById('demo').innerHTML = 'ERROR';
 }
